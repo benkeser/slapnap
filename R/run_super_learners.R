0a27f27270a72d1ebf497fb58a5644fa16ec4da3
Brian Williamson <brianw26@uw.edu>
2019-06-12 11:16:41 -0700

    add code to run all reduced groups

--- R/run_super_learners.R
+++ R/run_super_learners.R
@@ -2,11 +2,13 @@
 
 # load libraries
 library(SuperLearner)
+source("variable_groups.R")
 
 # attempt to read in environment variables
 reduce_covs <- Sys.getenv("reduce_covs") == "TRUE"
 reduce_outcomes <- Sys.getenv("reduce_outcomes") == "TRUE"
 reduce_library <- Sys.getenv("reduce_library") == "TRUE"
+reduce_groups <- Sys.getenv("reduce_groups") == "TRUE"
 
 # load data
 analysis_data_name <- list.files("/home/dat/analysis")
@@ -58,13 +60,13 @@ sl_one_outcome <- function(outcome_name,
         if (save_full_object) {
             saveRDS(fit, file = paste0(save_dir, fit_name))    
         } 
-        saveRDS(fit$SL.predict, file = paste0(save_dir, gsub(".RData", ".rds", gsub("fit", "fitted", fit_name))))
+        saveRDS(fit$SL.predict, file = paste0(save_dir, gsub(".RData", ".rds", gsub("fit_", "fitted_", fit_name))))
 
         cv_fit <- CV.SuperLearner(Y = dat[ , outcome_name], X = pred, ...)
         if (save_full_object) {
             saveRDS(cv_fit, file = paste0(save_dir, cv_fit_name))
         } 
-        saveRDS(cv_fit$SL.predict, file = paste0(save_dir, gsub(".RData", ".rds", gsub("cvfit", "cvfitted", cv_fit_name))))
+        saveRDS(cv_fit$SL.predict, file = paste0(save_dir, gsub(".RData", ".rds", gsub("cvfit_", "cvfitted_", cv_fit_name))))
         
         return(invisible(NULL))
 }
@@ -78,7 +80,34 @@ sl_ic50 <- sl_one_outcome(outcome_name = "pc.ic50",
                           method = "method.CC_LS",
                           reduce_covs = reduce_covs)
 ## run super learners on pre-defined groups
-
+all_var_groups <- get_variable_groups(dat, pred_names)
+if (reduce_groups) {
+    this_name <- names(all_var_groups)[1]
+    sl_ic50_i <- sl_one_outcome(outcome_name = "pc.ic50",
+                                    pred_names = pred_names[!(pred_names %in% all_var_groups[[1]])],
+                                    fit_name = paste0("fitted_pc.ic50_minus_", this_name, ".rds"),
+                                    cv_fit_name = paste0("cvfitted_pc.ic50_minus_", this_name, ".rds"),
+                                    family = "gaussian",
+                                    SL.library = SL.library,
+                                    cvControl = list(V = 10),
+                                    method = "method.CC_LS",
+                                    reduce_covs = reduce_covs,
+                                    save_full_object = FALSE)
+} else {
+    for (i in 1:length(all_var_groups)) {
+        this_name <- names(all_var_groups)[i]
+        sl_ic50_i <- sl_one_outcome(outcome_name = "pc.ic50",
+                                    pred_names = pred_names[!(pred_names %in% all_var_groups[[i]])],
+                                    fit_name = paste0("fitted_pc.ic50_minus_", this_name, ".rds"),
+                                    cv_fit_name = paste0("cvfitted_pc.ic50_minus_", this_name, ".rds"),
+                                    family = "gaussian",
+                                    SL.library = SL.library,
+                                    cvControl = list(V = 10),
+                                    method = "method.CC_LS",
+                                    reduce_covs = reduce_covs,
+                                    save_full_object = FALSE)
+    }
+}
 
 if(!reduce_outcomes){
   sl_ic80 <- sl_one_outcome(outcome_name = "pc.ic80",
@@ -87,25 +116,74 @@ if(!reduce_outcomes){
                            SL.library = SL.library, 
                            cvControl = list(V = 10),
                            method = "method.CC_LS")
-
+  for (i in 1:length(all_var_groups)) {
+        this_name <- names(all_var_groups)[i]
+        sl_ic80_i <- sl_one_outcome(outcome_name = "pc.ic80",
+                                    pred_names = pred_names[!(pred_names %in% all_var_groups[[i]])],
+                                    fit_name = paste0("fitted_pc.ic80_minus_", this_name, ".rds"),
+                                    cv_fit_name = paste0("cvfitted_pc.ic80_minus_", this_name, ".rds"),
+                                    family = "gaussian",
+                                    SL.library = SL.library,
+                                    cvControl = list(V = 10),
+                                    method = "method.CC_LS",
+                                    reduce_covs = reduce_covs,
+                                    save_full_object = FALSE)
+    }
   sl_iip <- sl_one_outcome(outcome_name = "iip",
                             pred_names = pred_names,
                            family = "gaussian",
                            SL.library = SL.library, 
                            cvControl = list(V = 10),
                            method = "method.CC_LS")
-
+  for (i in 1:length(all_var_groups)) {
+        this_name <- names(all_var_groups)[i]
+        sl_iip_i <- sl_one_outcome(outcome_name = "iip",
+                                    pred_names = pred_names[!(pred_names %in% all_var_groups[[i]])],
+                                    fit_name = paste0("fitted_iip_minus_", this_name, ".rds"),
+                                    cv_fit_name = paste0("cvfitted_iip_minus_", this_name, ".rds"),
+                                    family = "gaussian",
+                                    SL.library = SL.library,
+                                    cvControl = list(V = 10),
+                                    method = "method.CC_LS",
+                                    reduce_covs = reduce_covs,
+                                    save_full_object = FALSE)
+    }
   sl_dichotomous1 <- sl_one_outcome(outcome_name = "dichotomous.1",
                             pred_names = pred_names,
                            family = "binomial",
                            SL.library = SL.library, 
                            cvControl = list(V = 10),
                            method = "method.CC_nloglik")
-
+  for (i in 1:length(all_var_groups)) {
+        this_name <- names(all_var_groups)[i]
+        sl_dichotomous1_i <- sl_one_outcome(outcome_name = "dichotomous.1",
+                                    pred_names = pred_names[!(pred_names %in% all_var_groups[[i]])],
+                                    fit_name = paste0("fitted_dichotomous.1_minus_", this_name, ".rds"),
+                                    cv_fit_name = paste0("cvfitted_dichotomous.1_minus_", this_name, ".rds"),
+                                    family = "binomial",
+                                    SL.library = SL.library,
+                                    cvControl = list(V = 10),
+                                    method = "method.CC_nloglik",
+                                    reduce_covs = reduce_covs,
+                                    save_full_object = FALSE)
+    }
   sl_dichotomous2 <- sl_one_outcome(outcome_name = "dichotomous.2",
                             pred_names = pred_names,
                            family = "binomial",
                            SL.library = SL.library, 
                            cvControl = list(V = 10),
                          method = "method.CC_nloglik")
+  for (i in 1:length(all_var_groups)) {
+        this_name <- names(all_var_groups)[i]
+        sl_dichotomous2_i <- sl_one_outcome(outcome_name = "dichotomous.2",
+                                    pred_names = pred_names[!(pred_names %in% all_var_groups[[i]])],
+                                    fit_name = paste0("fitted_dichotomous.2_minus_", this_name, ".rds"),
+                                    cv_fit_name = paste0("cvfitted_dichotomous.2_minus_", this_name, ".rds"),
+                                    family = "binomial",
+                                    SL.library = SL.library,
+                                    cvControl = list(V = 10),
+                                    method = "method.CC_nloglik",
+                                    reduce_covs = reduce_covs,
+                                    save_full_object = FALSE)
+    }
 }
