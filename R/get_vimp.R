51e1a0e87956d607033a2714e86394d6ede33fda
Brian Williamson <brianw26@uw.edu>
2019-06-19 15:48:40 -0700

    change how vimp is done

--- R/get_vimp.R
+++ R/get_vimp.R
@@ -19,37 +19,45 @@ dat <- read.csv(paste0("/home/dat/analysis/", analysis_data_name), header = TRUE
 dat <- dat[complete.cases(dat),]
 
 # get pre-defined groups
+# get names of predictors
+non_pred_names <- c("pc.ic50", "pc.ic80", "iip",
+                    "dichotomous.1", "dichotomous.2",
+                    "seq.id.lanl","seq.id.catnap")
+pred_names <- colnames(dat)[!(colnames(dat) %in% non_pred_names)]
 all_var_groups <- get_variable_groups(dat, pred_names)
 
 # load all superlearner fits
 sl_fit_names <- list.files("/home/slfits")
-all_sl_fits <- lapply(as.list(sl_fit_names), readRDS)
-full_sl_fits <- all_sl_fits[grepl("fit_", sl_fit_names)]
-full_cv_sl_fits <- all_sl_fits[grepl("cvfit_", sl_fit_names)]
-reduced_sl_fits <- all_sl_fits[grepl("fitted_", sl_fit_names)]
-reduced_cv_sl_fits <- all_sl_fits[grepl("cvfitted_", sl_fit_names)]
-full_cv_sl_folds <- all_sl_fits[grepl("cvfolds_", sl_fit_names)]
+sl_fitted_names <- paste0("/home/slfits/", sl_fit_names[!grepl("fit_", sl_fit_names) & !grepl("cvfit_", sl_fit_names)])
+all_sl_fits <- lapply(as.list(sl_fitted_names), readRDS)
+full_sl_fits <- all_sl_fits[grepl("fitted_", sl_fitted_names) & !grepl("cvfitted_", sl_fitted_names) & !grepl("minus", sl_fitted_names)]
+full_cv_sl_fits <- all_sl_fits[grepl("cvfitted_", sl_fitted_names) & !grepl("minus", sl_fitted_names)]
+reduced_sl_fits <- all_sl_fits[grepl("fitted_", sl_fitted_names) & !grepl("cvfitted_", sl_fitted_names) & grepl("minus", sl_fitted_names)]
+reduced_cv_sl_fits <- all_sl_fits[grepl("cvfitted_", sl_fitted_names) & grepl("minus", sl_fitted_names)]
+full_cv_sl_folds <- all_sl_fits[grepl("cvfolds_", sl_fit_names) & !grepl("minus", sl_fit_names)]
 
-continuous_outcomes <- c("pc.ic50", "pc.ic80", "iip")
-binary_outcomes <- c("dichotomous.1", "dichotomous.2")
-
-
-# for continuous outcomes, do r-squared
+## get the outcomes in the list
+all_outcome_var_lst <- unique(gsub(".rds", "", gsub("cv", "", gsub("fitted_", "", sl_fit_names[!grepl("fit_", sl_fit_names) & !grepl("folds", sl_fit_names) & !grepl(".RData", sl_fit_names)]))))
+all_outcome_lst <- all_outcome_var_lst[!grepl("minus", all_outcome_var_lst)]
+all_var_grp_lst <- all_outcome_var_lst[grepl("minus", all_outcome_var_lst)]
+# for continuous outcomes, do r-squared;
+continuous_outcomes <- all_outcome_lst[grepl("ic50", all_outcome_lst) | grepl("ic80", all_outcome_lst) | grepl("iip", all_outcome_lst)]
+continuous_grps <- tail(unlist(strsplit(all_var_grp_lst[grepl("ic50", all_outcome_lst) | grepl("ic80", all_outcome_lst) | grepl("iip", all_outcome_lst)], "_", fixed = TRUE)), n = 1)
 continuous_outcome_vimp <- vector("list", length = length(continuous_outcomes))
-continuous_outcome_cv_vimp <- vector("list", length = length(continuous_outcomes))
 set.seed(474747)
 for (i in 1:length(continuous_outcome_vimp)) {
     ## make sub-folds for non-cv
     sub_folds <- sample(1:2, length(dat[, continuous_outcomes[i]]), replace = TRUE, prob = c(0.5, 0.5))
 
-    continuous_outcome_vimp[[i]] <- vector("list", length = length(reduced_sl_fits))
+    continuous_outcome_vimp[[i]] <- vector("list", length = length(continuous_grps))
     for (j in 1:length(reduced_sl_fits)) {
-        continuous_outcome_vimp[[i]][[j]] <- vimp::vimp_rsquared(Y = dat[, continuous_outcomes[i]], 
+        continuous_outcome_vimp[[i]][[j]] <- vimp::vim(Y = dat[, continuous_outcomes[i]], 
                                                             f1 = full_sl_fits[[i]],
                                                             f2 = reduced_sl_fits[[j]],
-                                                            indx = ,
+                                                            indx = which(pred_names %in% unlist(all_var_groups[grepl(continuous_grps[j], names(all_var_groups))])),
                                                             run_regression = FALSE,
                                                             alpha = 0.05,
+                                                            type = "r_squared",
                                                             folds = sub_folds)
     }
     continuous_outcome_cv_vimp[[i]] <- vector("list", length = length(reduced_cv_sl_fits))
@@ -57,12 +65,15 @@ for (i in 1:length(continuous_outcome_vimp)) {
         continuous_outcome_cv_vimp[[i]][[j]] <- vimp::cv_vim(Y = dat[, continuous_outcomes[i]],
                                                              f1 = full_cv_sl_fits[[i]],
                                                              f2 = reduced_cv_sl_fits[[i]],
-                                                             indx = ,
+                                                             indx = which(pred_names %in% unlist(all_var_groups[grepl(continuous_grps[j], names(all_var_groups))])),
                                                              run_regression = FALSE,
                                                              alpha = 0.05,
+                                                             type = "r_squared",
                                                              folds = full_cv_sl_folds[[i]])
     }
 }
 
-# for binary outcomes, do AUC, accuracy, deviance
-binary_outcome_vimp <- vector("list", length = length(binary_outcomes))
+# for binary outcomes, do AUC, accuracy, deviance;
+binary_outcomes <- all_outcome_lst[grepl("dichotomous.1", all_outcome_lst) | grepl("dichotomous", all_outcome_lst) | grepl("iip", all_outcome_lst)]
+binary_grps <- all_var_grp_lst[grepl("ic50", all_outcome_lst) | grepl("ic80", all_outcome_lst) | grepl("iip", all_outcome_lst)]
+binary_outcome_vimp <- vector("list", length = length(continuous_outcomes))
