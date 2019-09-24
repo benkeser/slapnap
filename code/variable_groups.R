b434d4685004029ef4b5c2aad77a25dd54a0ec69
Brian Williamson <brianw26@uw.edu>
2019-09-24 09:11:49 -0700

    add variable groups from Adam

--- code/variable_groups.R
+++ code/variable_groups.R
@@ -12,21 +12,27 @@ get_variable_group <- function(pred_names, hxb2_sites) {
 
 get_variable_groups <- function(data, pred_names) {
     ## set up sites
-    CD4_binding_sites <- c(124, 125, 126, 127, 196, 198, 279, 280, 281, 282, 283, 365, 366, 367, 368, 369, 370, 374, 425, 426, 427, 428, 429, 430, 431, 432, 455, 456, 457, 458, 459, 460, 461, 469, 471, 472, 473, 474, 475, 476, 477)
-    PNG_sites <- c(130, 139, 143, 156, 187, 197, 241, 262, 289, 339, 355, 363, 406, 408, 410, 442, 448, 460, 462)
+    ## CD4bs is feature group 2 from VRC01 paper plus additional sites identified by Adam
+    gp120_cd4bs <- unique(c(c(124, 125, 126, 127, 196, 198, 279, 280, 281, 282, 283, 365, 366, 367, 368, 369, 370, 374, 425, 426, 427, 428, 429, 430, 431, 432, 455, 456, 457, 458, 459, 460, 461, 469, 471, 472, 473, 474, 475, 476, 477), c(197, 209, 279, 326, 369, 119, 120, 182, 204, 206, 207, 274, 304, 318, 369, 471), c(62, 64, 66, 207), c(61, 64, 197, 276, 362, 363, 386, 392, 462, 463)))
+    gp120_v2_v2g_v2apex <- unique(c(157:196, c(121, 123, 124, 127, 197, 202, 203, 312, 315)))
+    gp120_v3_v3g <- unique(c(296:334), c(380, 406, 408, 415, 419, 428, 441, 443, 471), c(156, 137))
+    gp41_mper <- c(656:684, 609)
     all_glycosylationvars <- c('sequons.total.subset','sequons.total.env','sequons.total.gp120','sequons.total.v5','sequons.total.loop.d','sequons.total.loop.e','sequons.total.vrc01','sequons.total.cd4','sequons.total.sj.fence','sequons.total.sj.trimer')
     all_cysteinesvars <- c('cysteines.total.env','cysteines.total.gp120','cysteines.total.v5','cysteines.total.loop.d','cysteines.total.loop.e','cysteines.total.vrc01','cysteines.total.cd4')
     all_viralgeometryvars <- c('length.env','length.gp120','length.v5','length.v5.outliers','length.loop.d','length.loop.e','length.loop.e.outliers')
     all_stericbulkvars <- c('taylor.small.total.v5','taylor.small.total.loop.d','taylor.small.total.cd4')
 
     ## get all variable groups
-    aa_cd4bs_vars <- get_variable_group(pred_names, CD4_binding_sites)
-    aa_pngs_vars <- get_variable_group(pred_names, PNG_sites)
+    aa_gp120_cd4bs_vars <- get_variable_group(pred_names, gp120_cd4bs)
+    aa_gp120_v2_vars <- get_variable_group(pred_names, gp120_v2_v2g_v2apex)
+    aa_gp120_v3_vars <- get_variable_group(pred_names, gp120_v3_v3g)
+    aa_gp41_mper_vars <- get_variable_group(pred_names, gp41_mper)
     aa_glyco_vars <- all_glycosylationvars[all_glycosylationvars %in% colnames(data)]
     aa_cysteine_vars <- all_cysteinesvars[all_cysteinesvars %in% colnames(data)]
     aa_geometry_vars <- all_viralgeometryvars[all_viralgeometryvars %in% colnames(data)]
     aa_stericbulk_vars <- all_stericbulkvars[all_stericbulkvars %in% colnames(data)]
-    return(list(cd4bs = aa_cd4bs_vars, pngs = aa_pngs_vars,
+    return(list(gp120_cd4bs = aa_gp120_cd4bs_vars, gp120_v2 = aa_gp120_v2_vars,
+                gp120_v3 = aa_gp120_v3_vars, gp41_mper = aa_gp41_mper_vars,
                 glyco = aa_glyco_vars, cysteines = aa_cysteine_vars,
                 geometry = aa_geometry_vars, stericbulk = aa_stericbulk_vars))
 }
