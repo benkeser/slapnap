14e8ff3a1b0efe030ede1679f557d3db918512f3
Benkeser <benkeser@emory.edu>
2020-02-26 17:13:14 -0500

    move imp measures out of preamble into separate files

---
+++ code/biological_importance.R
@@ -0,0 +1,55 @@
+
+## ----------------------------------------------------------------------------
+## Population variable importance
+## ----------------------------------------------------------------------------
+num_pop_import <- 20  # the number of individual features to display in plots
+
+## plotting things
+x_lab_continuous <- expression(paste("Difference in ", R^2, sep = ""))
+x_lim_continuous <- c(0, 1.2)
+x_lab_binary <- expression(paste("Difference in ", AUC, sep = ""))
+x_lim_binary <- c(0, 1.2)
+## read in importance results for each outcome, create a plot for each
+## only return non-cv plots if cv = FALSE
+imp_nms <- list(all_var_groups, all_var_groups, var_inds)
+for (i in 1:length(outcome_names)) {
+    this_outcome_name <- outcome_names[i]
+    if (grepl("dichot", this_outcome_name)) {
+        this_x_lab <- x_lab_binary
+        this_x_lim <- x_lim_binary
+    } else {
+        this_x_lab <- x_lab_continuous
+        this_x_lim <- x_lim_continuous
+    }
+    ## importance results
+    eval(parse(text = paste0(this_outcome_name, "_vimp_lst <- readRDS(file = paste0(slfits_dir, '", this_outcome_name, "_vimp.rds'))")))
+    eval(parse(text = paste0(this_outcome_name, "_cv_vimp_lst <- readRDS(file = paste0(slfits_dir, '", this_outcome_name, "_cv_vimp.rds'))")))
+    eval(parse(text = paste0(this_outcome_name, "_outer_folds <- readRDS(file = paste0(slfits_dir, '", this_outcome_name, "_outer_folds.rds'))")))
+    eval(parse(text = paste0("num_obs_full <- sum(", this_outcome_name, "_outer_folds == 1)")))
+    eval(parse(text = paste0("num_obs_red <- sum(", this_outcome_name, "_outer_folds == 2)")))
+    ## make plots
+    eval(parse(text = paste0("current_vimp_lst <- ", this_outcome_name, "_vimp_lst")))
+    eval(parse(text = paste0("current_cv_vimp_lst <- ", this_outcome_name, "_cv_vimp_lst")))
+    vimp_plot_titles <- paste0(vimp_plot_name(this_outcome_name), ": ", unlist(lapply(as.list(names(current_vimp_lst)), vimp_plot_type)))
+    eval(parse(text = paste0(this_outcome_name, "_vimp_plots <- mapply(function(x, y) plot_one_vimp(x, title = y, x_lab = this_x_lab, x_lim = this_x_lim, cv = FALSE, num_plot = num_pop_import), current_vimp_lst, vimp_plot_titles, SIMPLIFY = FALSE)")))
+    eval(parse(text = paste0(this_outcome_name, "_cv_vimp_plots <- mapply(function(x, y) plot_one_vimp(x, title = y, x_lab = this_x_lab, x_lim = this_x_lim, cv = TRUE, num_plot = num_pop_import), current_cv_vimp_lst, vimp_plot_titles, SIMPLIFY = FALSE)")))
+}
+
+## make table for executive summary
+vimp_threshold <- 0.05
+if (opts$cvperf) {
+    vimp_summary_tbl <- make_vimp_executive_summary_table(
+        switch("log10.pc.ic50" %in% outcome_names, log10.pc.ic50_cv_vimp_lst, NULL),
+        switch("log10.pc.ic80" %in% outcome_names, log10.pc.ic80_cv_vimp_lst, NULL),
+        switch("iip" %in% outcome_names, iip_cv_vimp_lst, NULL),
+        switch("dichotomous.1" %in% outcome_names, dichotomous.1_cv_vimp_lst, NULL),
+        switch("dichotomous.2" %in% outcome_names, dichotomous.2_cv_vimp_lst, NULL),
+        threshold = vimp_threshold, outcome_names = outcome_names, cv = TRUE, opts = opts)
+} else {
+    vimp_summary_tbl <- make_vimp_executive_summary_table(switch("log10.pc.ic50" %in% outcome_names, log10.pc.ic50_vimp_lst, NULL),
+    switch("log10.pc.ic80" %in% outcome_names, log10.pc.ic80_vimp_lst, NULL),
+    switch("iip" %in% outcome_names, iip_vimp_lst, NULL),
+    switch("dichotomous.1" %in% outcome_names, dichotomous.1_vimp_lst, NULL),
+    switch("dichotomous.2" %in% outcome_names, dichotomous.2_vimp_lst, NULL),
+    threshold = vimp_threshold, outcome_names = outcome_names, cv = FALSE, opts = opts)
+}
---
+++ code/pred_importance.R
@@ -0,0 +1,59 @@
+
+## ----------------------------------------------------------------------------
+## Individual-level algorithm-specific importance
+## ----------------------------------------------------------------------------
+# get individual p-value-based importance measures; create tables
+col_idx <- geog_idx:ncol(dat)
+max_features <- 15
+if ("log10.pc.ic50" %in% outcome_names) {
+    imp_ic50 <- get_all_importance("log10.pc.ic50", binary_outcome = FALSE,
+                                   dir_loc = slfits_dir,
+                                   dat = dat, which_cols = col_idx, opts = opts)
+   ic50_tab <- get_importance_table(imp_ic50, max_features = max_features)
+   direction_resis <- get_importance_resis(ic50_tab, dat = dat, which_outcome = "log10.pc.ic50")
+   ic50_tab$direction <- ifelse(direction_resis, "Resistant", "Sensitive")
+}
+if ("log10.pc.ic80" %in% outcome_names) {
+    imp_ic80 <- get_all_importance("log10.pc.ic80", binary_outcome = FALSE,
+                                   dir_loc = slfits_dir,
+                                   dat = dat, which_cols = col_idx, opts = opts)
+   ic80_tab <- get_importance_table(imp_ic80, max_features = max_features)
+   direction_resis <- get_importance_resis(ic80_tab, dat = dat, which_outcome = "log10.pc.ic80")
+   ic80_tab$direction <- ifelse(direction_resis, "Resistant", "Sensitive")
+}
+if ("iip" %in% outcome_names) {
+    imp_iip <- get_all_importance("iip", binary_outcome = FALSE,
+                                   dir_loc = slfits_dir,
+                                   dat = dat, which_cols = col_idx, opts = opts)
+   iip_tab <- get_importance_table(imp_iip, max_features = max_features)
+   direction_resis <- get_importance_resis(iip_tab, dat = dat, which_outcome = "iip")
+   iip_tab$direction <- ifelse(direction_resis, "Resistant", "Sensitive")
+}
+if ("dichotomous.1" %in% outcome_names) {
+    imp_dichot1 <- get_all_importance("dichotomous.1", binary_outcome = TRUE,
+                                   dir_loc = slfits_dir,
+                                   dat = dat, which_cols = col_idx, opts = opts)
+   dichot1_tab <- get_importance_table(imp_dichot1, max_features = max_features)
+   direction_resis <- get_importance_resis(dichot1_tab, dat = dat, which_outcome = "dichotomous.1")
+   dichot1_tab$direction <- ifelse(direction_resis, "Resistant", "Sensitive")
+}
+if ("dichotomous.2" %in% outcome_names) {
+    imp_dichot2 <- get_all_importance("dichotomous.2", binary_outcome = TRUE,
+                                   dir_loc = slfits_dir,
+                                   dat = dat, which_cols = col_idx, opts = opts)
+   dichot2_tab <- get_importance_table(imp_dichot2, max_features = max_features)
+   direction_resis <- get_importance_resis(dichot2_tab, dat = dat, which_outcome = "dichotomous.2")
+   dichot2_tab$direction <- ifelse(direction_resis, "Resistant", "Sensitive")
+}
+continuous_imp_lst <- list(
+    switch("log10.pc.ic50" %in% outcome_names, ic50_tab, NULL),
+    switch("log10.pc.ic80" %in% outcome_names, ic80_tab, NULL),
+    switch("iip" %in% outcome_names, iip_tab, NULL)
+)
+dichot_imp_lst <- list(
+    switch("dichotomous.1" %in% outcome_names, dichot1_tab, NULL),
+    switch("dichotomous.2" %in% outcome_names, dichot2_tab, NULL)
+)
+imp_continuous <- combine_importance(continuous_imp_lst, out_names = c("IC50", "IC80", "IIP"))
+imp_dichot <- combine_importance(dichot_imp_lst, out_names = c("Estimated Sens.", "Multiple Sens."))
+imp_overall <- combine_importance(c(continuous_imp_lst, dichot_imp_lst))
