2ed10666a571e8a73b72593043968fc405799bf6
Brian Williamson <brianw26@uw.edu>
2020-03-23 14:26:13 -0700

    update unit tests, move summary.myCV.SuperLearner and get_est_and_ci to utils

--- code/utils.R
+++ code/utils.R
@@ -9,7 +9,7 @@ get_importance_text <- function(opts, imp_df, n_ft = 20){
     algo_with_highest_wt <- imp_df$algo[1]
 
     # check if super learner
-    is_sl <- length(opts$learners > 1)
+    is_sl <- length(opts$learners) > 1
     # check if tuning parameters varied
     is_tuned <- opts$cvtune
 
@@ -29,7 +29,7 @@ get_importance_text <- function(opts, imp_df, n_ft = 20){
         if(is_tuned){
             text_out <- paste0(text_out, " These ranks are shown for the choice of alpha that resulted in the best model fit, as chosen by cross-validation.")
         }
-        text_out <- paste0(text_out, " Overall, there were ", sum(abs(imp_df$value) > 0), " features that had non-zero coefficient in the final fit.")
+        text_out <- paste0(text_out, " Overall, there were ", sum(abs(imp_df$Importance) > 0), " features that had non-zero coefficient in the final fit.")
     }else{
         text_out <- "Specifically, the algorithm with the largest weight in the super learner ensemble was selected and associated variable importance metrics for this algorithm are shown."
         text_out <- paste0(text_out, " In this case, the highest weight was assigned to a ", algo_with_highest_wt, " algorithm, and thus the variable importance measures presented correspond to ")
@@ -37,7 +37,7 @@ get_importance_text <- function(opts, imp_df, n_ft = 20){
             text_out <- paste0(text_out, "random forest variable permutation-based variable importance measures were computed and are shown by their rank. The permutation-based importance measures the decrease in predictive accuracy when making out-of-bag predictions and randomly permuting a given feature from its original values.")
         }else if(algo_with_highest_wt == "lasso"){
             text_out <- paste0(text_out, "the magnitude of the coefficient for the model with $lambda$ chosen via cross-validation.")
-            text_out <- paste0(text_out, " Overall, there were ", sum(abs(imp_df$value) > 0), " features that had non-zero coefficient in the final lasso fit.")
+            text_out <- paste0(text_out, " Overall, there were ", sum(abs(imp_df$Importance) > 0), " features that had non-zero coefficient in the final lasso fit.")
         }else if(algo_with_highest_wt == "xgboost"){
             text_out <- paste0(text_out, "xgboost gain importance measures were computed and are shown by their rank. Gain measures the improvement in accuracy brought by a given feature to the tree branches on which it appears. The essential idea is that before adding a split on a given feature to the branch, there may be some observations that are poorly predicted, while after adding an additional split on this feature, and each resultant branch is more accurate. Gain measures this change in accuracy.")
         }
@@ -177,9 +177,9 @@ get_learner_descriptions <- function(opts){
 
 # each entry in the output list is a kable that should be properly labeled.
 get_cv_outcomes_tables <- function(fit_list, opts){
-    fit_list <- fit_list_out$out
-    V <- fit_list_out$V
-    n_row_now <- fit_list_out$n_row_now
+    fit_list <- fit_list$out
+    V <- fit_list$V
+    n_row_now <- fit_list$n_row_now
     table_list <- lapply(fit_list, summary.myCV.SuperLearner, opts = opts)
 
     # re-label
@@ -198,7 +198,7 @@ get_cv_outcomes_tables <- function(fit_list, opts){
         rsqtab <- Reduce(rbind, lapply(list_rows, unlist, use.names = FALSE))
         if(is.null(dim(rsqtab))) rsqtab <- matrix(rsqtab, nrow = 1)
         row.names(rsqtab) <- tmp[cont_idx]
-        rsq_kab <- kable(rsqtab, col.names = c(expression(CV-R^2), "Lower 95% CI", "Upper 95% CI"),
+        rsq_kab <- knitr::kable(rsqtab, col.names = c(expression(CV-R^2), "Lower 95% CI", "Upper 95% CI"),
               digits = 3, row.names = TRUE,
               caption = paste0("Estimates of ", V, "-fold cross-validated R-squared for super learner predictions ",
                                "of the three continuous-valued outcomes (n = ", n_row_now,
@@ -212,7 +212,7 @@ get_cv_outcomes_tables <- function(fit_list, opts){
         auctab <- Reduce(rbind, lapply(list_rows, unlist, use.names = FALSE))
         if(is.null(dim(auctab))) auctab <- matrix(auctab, nrow = 1)
         row.names(auctab) <- tmp[dich_idx]
-        auc_kab <- kable(auctab, col.names = c("CVAUC", "Lower 95% CI", "Upper 95% CI"),
+        auc_kab <- knitr::kable(auctab, col.names = c("CVAUC", "Lower 95% CI", "Upper 95% CI"),
               digits = 3, row.names = TRUE,
               caption = paste0("Estimates of ", V, "-fold cross-validated R-squared for super learner predictions ",
                                "of the three continuous-valued outcomes (n = ", n_row_now,
@@ -427,8 +427,8 @@ vimp_nice_group_names <- function(nm_vec) {
     return(nice_names[reference_positions])
 }
 vimp_nice_ind_names <- function(nm_vec) {
-    no_hxb2 <- gsub("hxb2.", "", nm_vec)
-    no_1mer <- gsub(".1mer", "", nm_vec)
+    no_hxb2 <- gsub("hxb2.", "", nm_vec, fixed = TRUE)
+    no_1mer <- gsub(".1mer", "", no_hxb2, fixed = TRUE)
     return(no_1mer)
 }
 ## nice plotting names
@@ -459,4 +459,195 @@ vimp_nice_rownames <- function(vimp_obj, cv = FALSE) {
     }
     tmp_nms <- unlist(lapply(strsplit(names(lst_s), "_", fixed = TRUE), function(x) paste(x[paste_ind:length(x)], collapse = "_")))
     return(tmp_nms[indx_mat])
+}
+
+# this function takes as input a fitted object EITHER of class SuperLearner OR
+# of class CV.SuperLearner and computes a summary table of performance.
+# if all object$Y are 0/1, it will compute AUC; otherwise it computes R^2
+# if SuperLearner is used to evaluate CV performance of a single algorithm,
+# a single row table is returned with that algorithms performance.
+# if CV.SuperLearner is used to evaluate CV performance of a CV-tuned single algorithm,
+# a table with a row for each choice of tuning parameters and for the cv-selected tuning
+# parameters is returned.
+# if CV.SuperLearner is used to evaluated CV performance of SuperLearner, then an
+# additional row is added that describes the performance of SuperLearner.
+summary.myCV.SuperLearner <- function (object, obsWeights = NULL, method = NULL, opts, ...) {
+    if ("env" %in% names(object)) {
+        env = object$env
+    }else {
+        env = parent.frame()
+    }
+
+    is_sl <- "SuperLearner" %in% class(object)
+    is_cvsl <- "CV.SuperLearner" %in% class(object)
+    if(is_sl | is_cvsl){
+      library.names <- object$libraryNames
+      if(is_cvsl){
+        V <- object$V
+      }else{
+        V <- length(object$validRows)
+      }
+      n <- length(object$SL.predict)
+      if (is.null(obsWeights)) {
+        obsWeights <- rep(1, length(object$Y))
+      }
+
+      if(is_cvsl){
+        folds <- object$folds
+      }else if(is_sl){
+        folds <- object$validRows
+      }
+
+      if(is_cvsl){
+        # only will use this if multiple learners selected
+        SL.predict <- object$SL.predict
+        # this will be only output if single learner used and opts$cvtune
+        discreteSL.predict <- object$discreteSL.predict
+        # only will use this if multiple learners selected
+        library.predict <- object$library.predict
+      }else if(is_sl){
+        # in this case a single "default" learner was requested
+        # so we can pull Z out from the object
+        SL.predict <- object$Z[,1]
+      }
+
+      Y <- object$Y
+      Risk.SL <- rep(NA, length = V)
+      se.SL <- rep(NA, length = V)
+      if(is_cvsl){
+        Risk.dSL <- rep(NA, length = V)
+        se.dSL <- rep(NA, length = V)
+        Risk.library <- matrix(NA, nrow = length(library.names),
+            ncol = V)
+        se.library <- matrix(NA, nrow = length(library.names),
+            ncol = V)
+        rownames(Risk.library) <- library.names
+      }
+      if (!(all(Y %in% c(0,1)))) {
+          for (ii in seq_len(V)) {
+              Risk.SL[ii] <- mean(obsWeights[folds[[ii]]] * (Y[folds[[ii]]] -
+                  SL.predict[folds[[ii]]])^2)
+              if(is_cvsl){
+                Risk.dSL[ii] <- mean(obsWeights[folds[[ii]]] * (Y[folds[[ii]]] -
+                    discreteSL.predict[folds[[ii]]])^2)
+                Risk.library[, ii] <- apply(library.predict[folds[[ii]],
+                    , drop = FALSE], 2, function(x) mean(obsWeights[folds[[ii]]] *
+                    (Y[folds[[ii]]] - x)^2))
+              }
+          }
+          if_sl <- (Y - SL.predict)^2 - mean((Y - SL.predict)^2)
+          if(is_cvsl){
+            if_dsl <- (Y - discreteSL.predict)^2 - mean((Y - discreteSL.predict)^2)
+            if_library <- apply(library.predict, 2, function(x){ (Y - x)^2 - mean((Y - x)^2) })
+          }
+          if_varY <- (Y - mean(Y))^2 - mean((Y - mean(Y))^2)
+          get_log_se <- function(if_risk, if_varY, risk, varY,
+                                 n = length(if_risk)){
+              grad <- matrix(c(1 / risk, - 1 /varY), nrow = 2)
+              Sig <- cov(cbind(if_risk, if_varY))
+              se_log <- t(grad) %*% Sig %*% grad
+              return(se_log)
+          }
+
+          if(is_cvsl){
+            if(length(opts$learners) > 1){
+              se <- (1/sqrt(n)) * c(
+                get_log_se(if_risk = if_sl, if_varY = if_varY, risk = mean(Risk.SL), varY = var(Y)),
+                get_log_se(if_risk = if_dsl, if_varY = if_varY, risk = mean(Risk.dSL), varY = var(Y)),
+                mapply(if1 = split(if_library, col(if_library)), risk = split(Risk.library, row(Risk.library)),
+                       function(if1, risk){ get_log_se(if_risk = if1, if_varY = if_varY, risk = mean(risk), varY = var(Y))})
+              )
+              Table <- data.frame(Algorithm = c("Super Learner", "Discrete SL",
+              library.names), Ave = c(1 - mean(Risk.SL)/var(Y), 1 - mean(Risk.dSL)/var(Y),
+              apply(Risk.library, 1, function(x){ 1 - mean(x)/var(Y) })), log_se = se, Min = c(min(1 - Risk.SL/var(Y)),
+              min(1 - Risk.dSL/var(Y)), apply(Risk.library, 1, function(x){ min(1 - mean(x)/var(Y))})), Max = c(max(1 - Risk.SL/var(Y)),
+              max(1 - Risk.dSL/var(Y)), apply(Risk.library, 1, function(x){ max(1 - mean(x)/var(Y)) })))
+
+            }else{
+              se <- (1/sqrt(n)) * c(
+                get_log_se(if_risk = if_dsl, if_varY = if_varY, risk = mean(Risk.dSL), varY = var(Y)),
+                mapply(if1 = split(if_library, col(if_library)), risk = split(Risk.library, row(Risk.library)),
+                       function(if1, risk){ get_log_se(if_risk = if1, if_varY = if_varY, risk = mean(risk), varY = var(Y))})
+              )
+
+              Table <- data.frame(Algorithm = c("Discrete SL",
+              library.names), Ave = c(1 - mean(Risk.dSL)/var(Y),
+              apply(Risk.library, 1, function(x){ 1 - mean(x)/var(Y) })), log_se = se,
+              Min = c(min(1 - Risk.dSL/var(Y)), apply(Risk.library, 1, function(x){ min(1 - mean(x)/var(Y))})),
+              Max = c(max(1 - Risk.dSL/var(Y)), apply(Risk.library, 1, function(x){ max(1 - mean(x)/var(Y)) })))
+            }
+          }else{
+            se <- (1/sqrt(n)) * get_log_se(if_risk = if_sl, if_varY = if_varY, risk = mean(Risk.SL), varY = var(Y))
+              Table <- data.frame(Algorithm = c(library.names[1]), Ave = c(1 - mean(Risk.SL)/var(Y)),
+                                  log_se = se,
+                                  Min = c(min(1 - Risk.SL/var(Y))),
+                                  Max = c(max(1 - Risk.SL/var(Y))))
+          }
+      }else {
+          requireNamespace("cvAUC")
+          for (ii in seq_len(V)) {
+            sl_auc <- cvAUC::ci.cvAUC(predictions = SL.predict[folds[[ii]]],
+                labels = Y[folds[[ii]]], folds = NULL)
+            Risk.SL[ii] <- sl_auc$cvAUC
+            se.SL[ii] <- sl_auc$se
+            if(is_cvsl){
+              dsl_auc <- cvAUC::ci.cvAUC(predictions = discreteSL.predict[folds[[ii]]],
+                  labels = Y[folds[[ii]]], folds = NULL)
+              Risk.dSL[ii] <- dsl_auc$cvAUC
+              se.dSL[ii] <- dsl_auc$se
+              library_auc <- apply(library.predict[folds[[ii]], , drop = FALSE], 2, function(x){
+                  tmp <- cvAUC::ci.cvAUC(predictions = x, labels = Y[folds[[ii]]], folds = NULL)
+                  return(c(tmp$cvAUC, tmp$se))
+                })
+              Risk.library[,ii] <- library_auc[1,]
+              se.library[,ii] <- library_auc[2,]
+            }
+          }
+          if(is_cvsl){
+            if(length(opts$learners) > 1){
+              se <- c(mean(se.SL, na.rm = TRUE), mean(se.dSL, na.rm = TRUE),
+                  rowMeans(se.library, na.rm = TRUE))
+              Table <- data.frame(Algorithm = c("Super Learner", "Discrete SL",
+                        library.names), Ave = c(mean(Risk.SL), mean(Risk.dSL),
+                        apply(Risk.library, 1, mean)), se = se, Min = c(min(Risk.SL),
+                        min(Risk.dSL), apply(Risk.library, 1, min)), Max = c(max(Risk.SL),
+                        max(Risk.dSL), apply(Risk.library, 1, max)))
+            }else{
+              se <- c(mean(se.dSL, na.rm = TRUE),
+                  rowMeans(se.library, na.rm = TRUE))
+              Table <- data.frame(Algorithm = c("Discrete SL",
+                        library.names), Ave = c(mean(Risk.dSL),
+                        apply(Risk.library, 1, mean)), se = se, Min = c(
+                        min(Risk.dSL), apply(Risk.library, 1, min)), Max = c(
+                        max(Risk.dSL), apply(Risk.library, 1, max)))
+            }
+          }else{
+            se <- c(mean(se.SL, na.rm = TRUE))
+              Table <- data.frame(Algorithm = c(library.names[1]),
+                                  Ave = c(mean(Risk.SL)), se = se,
+                                  Min = c(min(Risk.SL)),
+                                  Max = c(max(Risk.SL)))
+          }
+      }
+      out <- list(call = object$call, method = method, V = V, Table = Table)
+    }
+    class(out) <- "summary.myCV.SuperLearner"
+    return(out)
+}
+
+get_est_and_ci <- function(idx, fit_list, Rsquared = FALSE, constant = qnorm(0.975)){
+  cv_fit_table <- fit_list[[idx]]
+  Mean <- cv_fit_table$Table$Ave
+  if(Rsquared){
+    se <- cv_fit_table$Table$log_se
+    Lower <- 1 - exp( log(-Mean + 1) + constant * se)
+    Upper <- 1 - exp( log(-Mean + 1) - constant * se)
+  }else{
+    se <- cv_fit_table$Table$se
+    # put AUC CI on logit scale
+    grad <- 1 / (Mean - Mean^2)
+    logit_se <- sqrt(se^2 * grad^2)
+    Lower <- plogis(qlogis(Mean) - constant * logit_se); Upper <- plogis(qlogis(Mean) + constant * logit_se)
+  }
+  return(list(est = Mean[1], ci = c(Lower[1], Upper[1])))
 }
--- tests/test-utils.R
+++ tests/test-utils.R
@@ -6,12 +6,13 @@ library("dplyr")
 library("tibble")
 
 source("code/utils.R")
+source("code/super_learner_libraries.R")
 
 # set up general test opts
 opts <- list()
 opts$nab <- "VRC07-523-LS"
-opts$outcomes <- "ic50;ic80;iip;sens1;sens2"
-opts$learners <- "rf;xgboost;lasso"
+opts$outcomes <- c("ic50", "ic80", "iip", "sens1", "sens2")
+opts$learners <- c("rf", "lasso", "xgboost")
 opts$cvtune <- TRUE
 opts$cvperf <- TRUE
 opts$importance_grp <- ""
@@ -32,48 +33,48 @@ opts_xgb$learners <- "xgboost"
 opts_xgb_notune <- opts_xgb
 opts_xgb_notune$cvtune <- FALSE
 # set up general importance df
-imp_df <- tibble::tibble(algo = c("rf", "lasso", "xgboost", rank = c(1, 2, 3), Importance = c(1, 2, 3)))
-imp_df_lasso <- tibble::tibble(algo = c("lasso", "rf", "xgboost", rank = c(1, 2, 3), Importance = c(1, 2, 3)))
-imp_df_xgb <- tibble::tibble(algo = c("xgboost", "lasso", "rf", rank = c(1, 2, 3), Importance = c(1, 2, 3)))
+imp_df <- tibble::tibble(algo = c("rf", "lasso", "xgboost"), rank = c(1, 2, 3), Importance = c(1, 2, 3))
+imp_df_lasso <- tibble::tibble(algo = c("lasso", "rf", "xgboost"), rank = c(1, 2, 3), Importance = c(1, 2, 3))
+imp_df_xgb <- tibble::tibble(algo = c("xgboost", "lasso", "rf"), rank = c(1, 2, 3), Importance = c(1, 2, 3))
 
 Sys.setenv(nab = "VRC07-523-LS", outcomes = "ic50;ic80;iip;sens1;sens2", learners = "rf;xgboost;lasso", cvtune = TRUE, cvperf = TRUE, importance_grp = "", importance_ind = "", report_name = "", return_full_sl_obj = TRUE, return_analysis_dataset = TRUE)
 test_that("Getting options works", {
     global_opts <- get_global_options()
     expect_match(global_opts$nab, "VRC07-523-LS")
-    expect_match(global_opts$outcomes, "ic50")
+    expect_equal(global_opts$outcomes, c("ic50", "ic80", "iip", "sens1", "sens2"))
     expect_true(global_opts$cvtune)
 })
 test_that("Importance text works", {
     # when SL is used
-    expect_output(get_importance_text(opts, imp_df), "super learner", fixed = TRUE)
+    expect_match(get_importance_text(opts, imp_df), "super learner", fixed = TRUE)
     # only rf used
     #   without tuning
-    expect_output(get_importance_text(opts_rf_notune, imp_df), "random forest", fixed = TRUE)
+    expect_match(get_importance_text(opts_rf_notune, imp_df), "random forest", fixed = TRUE)
     #   with tuning
-    expect_output(get_importance_text(opts_rf, imp_df), "cross-validation", fixed = TRUE)
+    expect_match(get_importance_text(opts_rf, imp_df), "cross-validation", fixed = TRUE)
     # only xgb used
     #   without tuning
-    expect_output(get_importance_text(opts_xgb_notune, imp_df), "tree", fixed = TRUE)
+    expect_match(get_importance_text(opts_xgb_notune, imp_df), "tree", fixed = TRUE)
     #   with tuning
-    expect_output(get_importance_text(opts_xgb, imp_df), "cross-validation", fixed = TRUE)
+    expect_match(get_importance_text(opts_xgb, imp_df), "cross-validation", fixed = TRUE)
     # only lasso used
     #   without tuning
-    expect_output(get_importance_text(opts_lasso_notune, imp_df), "lasso", fixed = TRUE)
+    expect_match(get_importance_text(opts_lasso_notune, imp_df), "lasso", fixed = TRUE)
     #   with tuning
-    expect_output(get_importance_text(opts_lasso, imp_df), "cross-validation", fixed = TRUE)
+    expect_match(get_importance_text(opts_lasso, imp_df), "cross-validation", fixed = TRUE)
     # rf wins
-    expect_output(get_importance_text(opts_rf, imp_df), "out-of-bag", fixed = TRUE)
+    expect_match(get_importance_text(opts, imp_df), "out-of-bag", fixed = TRUE)
     # lasso wins
-    expect_output(get_importance_text(opts_lasso, imp_df_lasso), "coefficient in the final lasso fit", fixed = TRUE)
+    expect_match(get_importance_text(opts, imp_df_lasso), "coefficient in the final lasso fit", fixed = TRUE)
     # xgboost wins
-    expect_output(get_importance_text(opts_xgb, imp_df_xgb), "xgboost gain importnce measures", fixed = TRUE)
+    expect_match(get_importance_text(opts, imp_df_xgb), "xgboost gain importance measures", fixed = TRUE)
     # what happens with no importance df
-    expect_length(get_importance_text(opts), 0)
+    expect_equal(get_importance_text(opts), "")
 })
 
 opts_all_import <- opts
-opts_all_import$importance_grp <- "marg;cond"
-opts_all_import$importance_ind <- "marg;cond"
+opts_all_import$importance_grp <- c("marg", "cond")
+opts_all_import$importance_ind <- c("marg", "cond")
 opts_marg <- opts
 opts_marg$importance_grp <- "marg"
 opts_marg$importance_ind <- "marg"
@@ -82,13 +83,13 @@ opts_cond$importance_grp <- "cond"
 opts_cond$importance_ind <- "cond"
 test_that("Biological importance text works", {
     # with marginal and conditional
-    expect_output(get_biological_importance_plot_description(opts_all_import, grp = TRUE), "left-hand plot", fixed = TRUE)
+    expect_match(get_biological_importance_plot_description(opts_all_import, grp = TRUE), "left-hand plot", fixed = TRUE)
     # marginal only
-    expect_output(get_biological_importance_plot_description(opts_marg, grp = TRUE), "marginal importance", fixed = TRUE)
+    expect_match(get_biological_importance_plot_description(opts_marg, grp = TRUE), "marginal importance", fixed = TRUE)
     # with conditional only
-    expect_output(get_biological_importance_plot_description(opts_cond, grp = TRUE), "conditional importance", fixed = TRUE)
+    expect_match(get_biological_importance_plot_description(opts_cond, grp = TRUE), "conditional importance", fixed = TRUE)
     # with no importance
-    expect_output(get_biological_importance_plot_description(opts, grp = TRUE), "")
+    expect_equal(get_biological_importance_plot_description(opts, grp = TRUE), "")
 })
 
 ncomplete <- 100
@@ -96,13 +97,131 @@ num_obs_full <- 50
 num_obs_red <- 50
 test_that("Biological importance figure caption works", {
     # test outcomes
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic50", grp = TRUE), "IC-50")
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic80", grp = TRUE), "IC-80")
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "iip", grp = TRUE), "IIP")
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "sens1", grp = TRUE), "estimated sensitivity")
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "sens2", grp = TRUE), "multiple sensitivity")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic50", grp = TRUE), "IC-50")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic80", grp = TRUE), "IC-80")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "iip", grp = TRUE), "IIP")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "sens1", grp = TRUE), "estimated sensitivity")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "sens2", grp = TRUE), "multiple sensitivity")
     # test group
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic50", grp = TRUE), "Group")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic50", grp = TRUE), "Group")
     # test individual feature
-    expect_output(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic50", grp = FALSE), "Individual")
+    expect_match(biological_importance_figure_caption(ncomplete, num_obs_full, num_obs_red, "ic50", grp = FALSE), "Individual")
+})
+
+test_that("Individual nab summaries work", {
+
+})
+
+test_that("Learner descriptions work", {
+    expect_match(get_learner_descriptions(opts_rf), "with tuning parameters selected")
+    expect_match(get_learner_descriptions(opts_rf_notune), "with tuning parameters set to their")
+    expect_match(get_learner_descriptions(opts), "a super learner ensemble of several")
+})
+
+# generate some test data
+set.seed(4747)
+x <- replicate(10, rnorm(100, 0, 1))
+y <- rbinom(100, 1, exp(x)/(1+exp(x)))
+dat <- data.frame(dichot1 = y, x)
+sl_fit <- sl_one_outcome(dat, outcome_name = "dichot1", pred_names = paste0("X", 1:10), opts = opts, save_dir = "tests/", family = binomial(), SL.library = "SL.glmnet.0", cvControl = list(V = 5), method = "tmp_method.CC_nloglik")
+sl_fit <- readRDS("tests/fit_dichot1.rds")
+sl_fitted <- readRDS("tests/fitted_dichot1.rds")
+test_that("Run SL works", {
+    expect_equal(sl_fit$coef, 1)
+})
+
+opts_dichot1 <- opts
+opts_dichot1$outcomes <- "dichot1"
+sl_fit_lst <- load_cv_fits(opts, "tests/")
+test_that("CV outcome tables works", {
+    expect_equal(length(sl_fit_lst$out), 1)
+    cv_outcome_tbl_lst <- get_cv_outcomes_tables(sl_fit_lst, opts_dichot1)
+    expect_equal(length(cv_outcome_tbl_lst$r2), 0)
+    expect_output(print(cv_outcome_tbl_lst$auc), "CVAUC")
+})
+
+test_that("Outcome descriptions work") {
+    expect_match(get_outcome_descriptions(opts), "IIP is calculated as")
+    expect_match(get_outcome_descriptions(opts), "Estimated sensitivity is defined by")
+    expect_match(get_outcome_descriptions(opts), "Since only one antibody was specified for this analysis")
+    opts_2nab <- opts
+    opts_2nab$nab <- c("VRC07-523-LS", "PGT121")
+    expect_match(get_outcome_descriptions(opts_2nab), "Predicted IC-50")
+    expect_match(get_outcome_descriptions(opts_2nab), "additive model of Wagh et al")
+    expect_match(get_outcome_descriptions(opts_2nab), "Multiple sensitivity is defined as the binary indicator of")
+}
+
+test_that("Comma separated outcomes works", {
+    expect_equal(get_comma_sep_outcomes(opts), "IC-50, IC-80, IIP, estimated sensitivity, multiple sensitivity.")
+    expect_equal(get_comma_sep_outcomes(list(outcomes = "ic50")), "IC-50.")
+})
+
+set.seed(4747)
+test_that("Making CV folds works", {
+    y <- rbinom(100, 1, 0.5)
+    V <- 5
+    folds_1 <- make_folds(y, V, stratified = TRUE)
+    folds_2 <- make_folds(y, V, stratified = FALSE)
+    expect_equal(sum(folds_1 == 1), 21)
+    expect_equal(sum(folds_2 == 1), 20)
+})
+test_that("Getting SL folds works", {
+    folds_lst <- list(sample(1:5, 100, replace = TRUE), sample(1:5, 100, replace = TRUE))
+    expect_length(get_cv_folds(folds_lst), 200)
+})
+test_that("SL opts work", {
+    dichot_opts <- get_sl_options("dichot.1", 5)
+    cont_opts <- get_sl_options("log10.pc.ic50", 5)
+    expect_match(dichot_opts$method, "CC_nloglik")
+    expect_match(cont_opts$method, "CC_LS")
+    expect_equal(dichot_opts$fam, binomial())
+    expect_equal(cont_opts$fam, gaussian())
+})
+test_that("VIM options work", {
+    expect_match(get_vimp_options("ic50"), "r_squared")
+    expect_match(get_vimp_options("dichot.1"), "auc")
+})
+test_that("VIM list creation works", {
+    folds_lst <- list(sample(1:5, 100, replace = TRUE), sample(1:5, 100, replace = TRUE))
+    expect_length(make_vimp_list(), 4)
+    test_cv_lsts <- make_cv_lists(folds_lst, rnorm(200, 0, 1), rnorm(200, 0, 1))
+    expect_length(test_cv_lsts, 3)
+    expect_length(test_cv_lsts$folds, 200)
+    expect_length(test_cv_lsts$full_lst, 2)
+    expect_length(test_cv_lsts$redu_lst, 2)
+})
+test_that("Nice naming works for vimp", {
+    expect_match(vimp_nice_group_names("cysteines"), "Cysteine counts")
+    expect_length(vimp_nice_group_names("gp160"), 0)
+    expect_equal(vimp_nice_ind_names("hxb2.K.160.1mer"), "K.160")
+    expect_equal(vimp_plot_name("log10.pc.ic50"), "IC-50")
+    expect_equal(vimp_plot_name("dichotomous.2"), "Multiple sensitivity")
+    ## generate the data
+    ## generate X
+    p <- 10
+    n <- 100
+    x <- data.frame(replicate(p, stats::runif(n, -1, 1)))
+
+    ## apply the function to the x's
+    f <- function(x) 0.5 + 0.3*x[1] + 0.2*x[2]
+    smooth <- apply(x, 1, function(z) f(z))
+
+    ## generate Y ~ Normal (smooth, 1)
+    y <- matrix(rbinom(n, size = 1, prob = smooth))
+
+    ## set up a library for SuperLearner
+    learners <- "SL.gam"
+
+    ## using Y and X
+    folds <- sample(rep(seq_len(2), length = length(y)))
+    est <- vimp::vim(y, x, indx = c(1,2), type = "r_squared",
+               alpha = 0.05, run_regression = TRUE,
+               SL.library = learners, cvControl = list(V = 10),
+               folds = folds)
+    est_2 <- vimp::vim(y, x, indx = c(3, 4, 5, 6, 7, 8, 9, 10), type = "r_squared",
+                     alpha = 0.05, run_regression = TRUE,
+                     SL.library = learners, cvControl = list(V = 10),
+                     folds = folds)
+    all_est <- vimp::merge_vim(est, est_2)
+    expect_equal(vimp_nice_rownames(all_est, cv = TRUE), c("NA_NA_NA_est", "NA_NA_2"))
 })
