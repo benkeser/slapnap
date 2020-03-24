c73be6b8d9ae409f2a42f377ad790f72f64fc0ef
Brian Williamson <brianw26@uw.edu>
2020-03-24 09:57:39 -0700

    update local tests to name each report

--- run_tests_local.sh
+++ run_tests_local.sh
@@ -8,15 +8,26 @@ Rscript tests/test-check_opts.R
 # check utility functions
 Rscript tests/test-utils.R
 
+# set up local directory arg
+brian_computer=true
+if $brian_computer; then
+    local_dir=~/Projects/VIDD/hvtn/slapnap/
+else
+    # for David to set up
+    local_dir=~/Dropbox/Emory/AMP/slapnap/
+fi
+
+# only run if necessary
+# sudo docker build -t slapnap .
 # ------------------------------------------
 # Single nAb, single outcome, single learner
 # ------------------------------------------
-# no cv, no vimp, ic50
-sudo docker run -it \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_ic50_nocv_novimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+# no cv, no vimp, ic50; prints only descriptive statistics (no predictive performance) -- do we want that?
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_ic50_nocv_novimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS" \
     -e outcomes="ic50" \
     -e learners="lasso" \
@@ -26,14 +37,15 @@ sudo docker run -it \
     -e importance_ind="" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
-    slapnap bash
+    -e report_name="VRC07-523-LS_ic50_nocv_novimp" \
+    slapnap
 
 # no cv, no vimp, sens1
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_sens1_nocv_novimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_sens1_nocv_novimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS" \
     -e outcomes="sens1" \
     -e learners="lasso" \
@@ -43,14 +55,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS_sens1_nocv_novimp" \
     slapnap
 
 # no cv, vimp, ic50
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_ic50_nocv_vimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_ic50_nocv_vimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS" \
     -e outcomes="ic50" \
     -e learners="lasso" \
@@ -60,14 +73,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="pred" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS_ic50_nocv_vimp" \
     slapnap
 
 # cv, vimp, ic50
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_ic50_cv_vimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_ic50_cv_vimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS" \
     -e outcomes="ic50" \
     -e learners="lasso" \
@@ -77,17 +91,18 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="pred" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS_ic50_cv_vimp" \
     slapnap
 
 # ------------------------------------------
 # Multiple nAbs, single outcome, single learner
 # ------------------------------------------
 # no cv, no vimp, ic50
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_ic50_nocv_novimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_ic50_nocv_novimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="ic50" \
     -e learners="lasso" \
@@ -97,14 +112,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_ic50_nocv_novimp" \
     slapnap
 
 # no cv, no vimp, iip
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_iip_nocv_novimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_iip_nocv_novimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="iip" \
     -e learners="lasso" \
@@ -114,14 +130,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_iip_nocv_novimp" \
     slapnap
 
 # no cv, vimp
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_ic50_nocv_vimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_ic50_nocv_vimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="ic50" \
     -e learners="lasso" \
@@ -131,14 +148,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="pred" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_ic50_nocv_vimp" \
     slapnap
 
 # cv, vimp
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_ic50_cv_vimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_ic50_cv_vimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="ic50" \
     -e learners="lasso" \
@@ -148,17 +166,18 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="pred" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_ic50_cv_vimp" \
     slapnap
 
 # ------------------------------------------
 # Multiple nAbs, single outcome, multiple learners
 # ------------------------------------------
 # no cv, no vimp, ic50
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_multilearner_ic50_nocv_novimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_multilearner_ic50_nocv_novimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="ic50" \
     -e learners="lasso;rf" \
@@ -168,14 +187,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_ic50_nocv_novimp_multilearn" \
     slapnap
 
 # no cv, no vimp, iip
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_multilearner_iip_nocv_novimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_multilearner_iip_nocv_novimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="iip" \
     -e learners="lasso;rf" \
@@ -185,14 +205,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_iip_nocv_novimp_multilearn" \
     slapnap
 
 # no cv, vimp
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_multilearner_ic50_nocv_vimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_multilearner_ic50_nocv_vimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="ic50" \
     -e learners="lasso;rf" \
@@ -202,14 +223,15 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="pred" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_ic50_nocv_vimp_multilearn" \
     slapnap
 
 # cv, vimp
-sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/dat/analysis:/home/dat/analysis/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/code:/home/lib/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/slfits_multinab_multilearner_ic50_cv_vimp:/home/slfits/ \
-    -v ~/Projects/VIDD/hvtn/slapnap/sandbox/output:/home/output/ \
+sudo docker run \
+    -v $local_dir/sandbox/dat/analysis:/home/dat/analysis/ \
+    -v $local_dir/code:/home/lib/ \
+    -v $local_dir/sandbox/slfits_multinab_multilearner_ic50_cv_vimp:/home/slfits/ \
+    -v $local_dir/sandbox/output:/home/output/ \
     -e nab="VRC07-523-LS;PGT121" \
     -e outcomes="ic50" \
     -e learners="lasso;rf" \
@@ -219,4 +241,5 @@ sudo docker run -v ~/Projects/VIDD/hvtn/slapnap/sandbox:/home/ \
     -e importance_ind="pred" \
     -e return_full_sl_obj="FALSE" \
     -e return_analysis_dataset="FALSE" \
+    -e report_name="VRC07-523-LS;PGT121_ic50_cv_vimp_multilearn" \
     slapnap
