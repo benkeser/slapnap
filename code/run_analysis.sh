622916e724ac8778d4f9be52e1f676b0f020fc24
Brian Williamson <brianw26@uw.edu>
2020-04-24 10:24:05 -0700

    print all output to log file

--- code/run_analysis.sh
+++ code/run_analysis.sh
@@ -4,30 +4,45 @@
 # This script is executed as entry point to container
 #-----------------------------------------------------
 
+# set up a log file to print out to
+current_date=$(date "+%d%b%Y")
+log_file=($(echo "/home/output/"$nab"_"$current_date".log"))
+printf "Starting SLAPNAP \n"
+printf "Messages, warnings, and errors (if any) will appear in $log_file \n"
+
 # make sure that user-specified options match what we expect to see
 printf "Checking options \n"
-/home/lib/check_opts.R
+echo "--- Checking options --- \n" >> $log_file
+/home/lib/check_opts.R > $log_file
 
 # run script to build analytic data set
 printf "Building analytic data set from CATNAP database \n"
-/home/lib/merge_proc_v4.R
+echo "--- Building analytic data set from CATNAP database --- \n" >> $log_file
+/home/lib/merge_proc_v4.R > $log_file
 
 # run script to fit super learners
 printf "Fitting super learners \n"
-/home/lib/run_super_learners.R
+echo "--- Fitting super learners --- \n" >> $log_file
+/home/lib/run_super_learners.R > $log_file
 
 # run script to get variable importance
 printf "Estimating variable importance \n"
-/home/lib/get_vimp.R
+echo "--- Estimating variable importance --- \n" >> $log_file
+/home/lib/get_vimp.R > $log_file
 
 # run script to compile report
 printf "Compiling results using R Markdown \n"
-/home/lib/render_report.R
+echo "--- Compiling results using R Markdown --- \n" >> $log_file
+/home/lib/render_report.R > $log_file
 
 # return requested objects
 printf "Returning requested objects \n"
-/home/lib/return_requested_objects.R
+echo "--- Returning requested objects --- \n" >> $log_file
+/home/lib/return_requested_objects.R > $log_file
 
 # run script
 printf "Cleaning up \n"
+echo "--- Cleaning up --- \n" >> $log_file
 rm -rf /home/out/*_files
+
+echo "--- END --- \n" >> $log_file
