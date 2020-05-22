0839eb6b1546b8b78976195961b86984fd5f306c
David Benkeser <benkeser@emory.edu>
2020-05-22 18:14:52 -0400

    update date info. update build scripts to debug travis build

--- _build.sh
+++ _build.sh
@@ -1,4 +1,5 @@
-!# /bin/bash
+#! /bin/bash
 
+Rscript -e "getwd(); list.files('${TRAVIS_BUILD_DIR}')"
 Rscript -e "setwd('${TRAVIS_BUILD_DIR}/docs/'); bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "setwd('${TRAVIS_BUILD_DIR}/docs/'); bookdown::render_book('index.Rmd', 'bookdown::pdfbook')"
