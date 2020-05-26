a8e10b39c045520cddcd21efdbdbaeee58dbceba
David Benkeser <benkeser@emory.edu>
2020-05-26 13:44:38 -0400

    more robust file naming for html nginx

--- code/run_analysis.sh
+++ code/run_analysis.sh
@@ -54,7 +54,9 @@ if [[ "$view_port" == "TRUE" ]] && [[ "$return" == *"report"* ]]
 then
 printf "Report can be viewed on localhost. To stop container, retrieve CONTAINER ID using 'docker container ps' and 'docker stop CONTAINER_ID'.  \n"
 echo "--- Report can be viewed on localhost. ---" >> $log_file
-cp /home/output/*.html /var/www/html/index.html
+# copy report to www folder for viewing
+name_of_report=$(Rscript -e "antibody_string <- Sys.getenv('nab'); antibodies <- strsplit(gsub('/', '-', antibody_string), split = ';')[[1]]; report_name <- Sys.getenv('report_name'); current_date <- format(Sys.time(), '%d%b%Y'); if(report_name == '') report_name <- paste0('report_', paste (antibodies, collapse = '_'), '_', current_date); cat(report_name)")
+cp /home/output/$(name_of_report).html /var/www/html/index.html
 nginx -g "daemon off;"
 fi
 
