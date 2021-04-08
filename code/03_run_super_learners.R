0aaa5deaeec8150b8df563efd74fe8ebe142ccd4
bdwilliamson <brianw26@uw.edu>
2021-04-08 10:07:00 -0700

    Disable h2o progress bar

--- code/03_run_super_learners.R
+++ code/03_run_super_learners.R
@@ -29,6 +29,8 @@ h2o_here <- !(all(grepl("h2oboost", opts$learners) == FALSE))
 if(h2o_here) {
     # initiate h2o cluster
     h2o.init(max_mem_size = "32G")
+    # don't print the progress bar 
+    h2o::h2o.no_progress()
 }
 
 # load data and subset to complete cases
